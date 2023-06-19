1fc9644a44db1b1e6368da025bd797f634ae3cbe
argareeva <116667251+argareeva@users.noreply.github.com>
2023-06-19 17:41:46 +0500

    Add files via upload

---
+++ Microservice/Controllers/Menu.cs
@@ -0,0 +1,40 @@
+using Microservice.Context;
+using Microservice.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Menu : ControllerBase
+    {
+        private readonly DataBase _dbContext;
+
+        public Menu(DataBase dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/Menu
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Dish>>> GetMenu()
+        {
+            if (_dbContext.Dish == null)
+            {
+                return NotFound("No dishes yet");
+            }
+
+            var dishes = new List<Dish>();
+
+            foreach (var dish in _dbContext.Dish)
+            {
+                if (dish.IsAvailable)
+                {
+                    dishes.Add(dish);
+                }
+            }
+
+            return Ok(dishes);
+        }
+    }
+}
---
+++ Microservice/Tables/OrderDto.cs
@@ -0,0 +1,9 @@
+namespace Microservice.Tables;
+
+public class OrderDto
+{
+    public int UserId { get; set; }
+    public int DishId { get; set; }
+    public string Status { get; set; }
+    public string? SpecialRequests { get; set; }
+}
